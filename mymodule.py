0f27cb049bece7481668f24c61ee45ab4217f9c9
konstantin <konstantin.klein@hochfrequenz.de>
2021-03-10 10:04:35 +0100

    foo is blacklisted by pylint

--- mymodule.py
+++ mymodule.py
@@ -12,11 +12,11 @@ class MyClass:  # pylint: disable=too-few-public-methods
         """
         Initialize for the sake of initializing
         """
-        self.foo = "foo"
+        self.my_instance_var = "abc"
 
     def do_something(self) -> str:
         """
         Actually does nothing.
-        :return: the value of foo
+        :return: the value of an instance variable
         """
-        return self.foo
+        return self.my_instance_var
--- unittests/test_myclass.py
+++ unittests/test_myclass.py
@@ -3,9 +3,9 @@ from mymodule import MyClass
 
 class TestMyClass:
     """
-    A class with pytest unittests.
+    A class with pytest unit tests.
     """
 
     def test_something(self):
         my_class = MyClass()
-        assert my_class.do_something() == "foo"
+        assert my_class.do_something() == "my_instance_var"

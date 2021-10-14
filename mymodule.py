823b0612da6a6ed8cca5654f9d317b3eb96b7f7e
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-14 23:48:08 +0200

    positive

--- mymodule.py
+++ mymodule.py
@@ -1,7 +1,6 @@
 """
 This a docstring for the module.
 """
-from typing import Optional
 
 
 class MyClass:  # pylint: disable=too-few-public-methods
@@ -15,17 +14,10 @@ class MyClass:  # pylint: disable=too-few-public-methods
         """
         self.my_instance_var: str = "abc"
 
-    def do_something(self) -> Optional[list]:  # another wrong type hint for mypy to find
+    def do_something(self) -> int:  # either type hint or actual return type is wrong / let mypy find it.
         """
         Actually does nothing.
         :return: the value of an instance variable
         """
         # this is a super long line with: 100 < line length <= 120 to demonstrate the purpose of pyproject.toml
-        self.do_somthing_else(self.my_instance_var)  # something goes wrong here. let mypy find it
         return self.my_instance_var
-
-    def do_somthing_else(self, argument: int) -> None:
-        """
-        This method prints the argument
-        """
-        print(f"I'm doing something with '{argument}'...")

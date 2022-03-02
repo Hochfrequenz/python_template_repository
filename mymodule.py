a79cb0fd2beed3e5f76ba5b20578b5a00befb602
konstantin <konstantin.klein@hochfrequenz.de>
2022-03-02 18:21:25 +0100

    fix

--- mymodule.py
+++ mymodule.py
@@ -12,12 +12,13 @@ class MyClass:  # pylint: disable=too-few-public-methods
         """
         Initialize for the sake of initializing
         """
-        self.my_instance_var: str = "abc"
+        self.my_instance_var: int = "asd"
 
     def do_something(self) -> str:
         """
         Actually does nothing.
         :return: the value of an instance variable
         """
-        # this is a super long line with: 100 < line length <= 120 to demonstrate the purpose of pyproject.toml
-        return self.my_instance_var
+        #this  is a super long line with: 100 < line length <= 120 to demonstrate the purpose of pyproject.toml
+        return self.my_instance_var,
+    
